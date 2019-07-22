a1a497b402c3cf21cd55f1cff85f5049e80e6a12
crispycat <30805430+crispycat0@users.noreply.github.com>
2019-07-22 16:31:24 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Media;
+using System.Windows;
+
+namespace Bruh {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            new SoundPlayer("/Bruh;component/bruh.mp3").Play();  
+        }
+    }
+}
